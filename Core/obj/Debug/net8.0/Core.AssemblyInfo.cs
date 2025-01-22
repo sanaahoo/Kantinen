0b58f778c3920e547a46ddd96ecfe07e75b8621a
sanaahoo <167194847+sanaahoo@users.noreply.github.com>
2025-01-22 22:55:59 +0100

    LoginPage Css

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9318425c61030cb033a5ba8ce518b4486d010541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda4331bf39eef005b4b41a4b3a074f5d62a4cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KantinenAPI/obj/Debug/net8.0/KantinenAPI.AssemblyInfo.cs
+++ KantinenAPI/obj/Debug/net8.0/KantinenAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KantinenAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9318425c61030cb033a5ba8ce518b4486d010541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda4331bf39eef005b4b41a4b3a074f5d62a4cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("KantinenAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KantinenAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
